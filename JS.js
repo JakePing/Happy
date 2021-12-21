68bbf63c95b4e28588b51df57469e6dd62ea6d61
JakePing <96445385+JakePing@users.noreply.github.com>
2021-12-21 14:23:53 +0300

    Delete JS.js

--- JS.js
+++
@@ -1,8 +0,0 @@
-function my_proverka() {
-  var code = document.getElementById("my_pass").value;
-  var pass = "0000";
-  if (code == pass) {
-    var win = document.getElementById("openModal");
-    win.classList.add("close");
-  }
-} 
